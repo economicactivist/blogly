5e339ff1b20489db739a76e03d4e0266224f9a70
Mountain Scott <mountainscott@yahoo.com>
2021-06-28 23:14:04 -0700

    finished but deleted ds_store file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, request, render_template, redirect, flash, session, url_for
 from flask.helpers import url_for
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, db_connect
+from models import db, db_connect, User
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -14,6 +14,7 @@ app.config['SECRET_KEY'] = "KJOJKLJKJLKJL"
 debug = DebugToolbarExtension(app)
 
 db_connect(app)
+db.drop_all()
 db.create_all()
 
 
@@ -23,7 +24,8 @@ def redirect_to_user_list():
 
 @app.route('/users')
 def list_users():
-    return render_template('user-list.html')
+    users = User.query.all()
+    return render_template('user-list.html', users=users)
 
 @app.route('users/new')
 def add_new_user():
@@ -31,24 +33,42 @@ def add_new_user():
 
 @app.route('users/new', methods=['POST'])
 def add_new_user_to_db():
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    image_url = request.form["img_url"]
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit(new_user)
     #####################
     return redirect(url_for('list_users'))
 
 @app.route('/users/<int:user_id>')
 def show_user_detail(user_id):
-    return render_template(url_for('user-detail.html', user_id=user_id))
+    user = User.query.get_or_404(user_id)
+    return render_template(url_for('user-detail.html', user=user))
 
 @app.route('/users/<int:user_id>/edit')
-def edit_user(user_id):
-    return render_template(url_for('edit-user.html'))
+def edit_user(user):
+    #maybe I should just pass user here
+   
+    return render_template(url_for('edit-user.html', user=user))
 
 @app.route('/users/<int:user_id>/edit', methods=['POST'])
 def post_user_edits_to_db(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form["first_name"]
+    user.last_name = request.form["last_name"]
+    user.image_url = request.form["img_url"]
+    db.session.add(user)
+    db.session.commit(user)
     return redirect(url_for('list_users'))
 
 @app.route('/users/<int:user_id>/delete')
 def delete_user(user_id):
     flash('User Deleted')
+    user_id = int(user_id)
+    User.query.filter_by(id=user_id).delete()
     return redirect(url_for('list_users'))
 
 
